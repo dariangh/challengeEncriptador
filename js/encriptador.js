cc2a4af2ac794e84048659df80b1d14382e8be40
Darian Gutierrez <dariangutierrez01@gmail.com>
2024-03-05 06:47:37 -0600

    challenge encriptador

---
+++ js/encriptador.js
@@ -0,0 +1,145 @@
+function diccionarios(){
+    return{
+    reemplazosEncriptar: {
+        e: "enter",
+        i: "imes",
+        a: "ai",
+        o: "ober",
+        u: "ufat",
+    },
+    reemplazosDesencriptar: {
+        enter: "e",
+        imes:  "i",
+        ai:    "a",
+        ober:  "o",
+        ufat:  "u",
+    },
+};
+}
+//limpiar caja
+function limpiarEncriptarCaja() {
+    document.querySelector(".entrada-texto").value="";
+    let imgLimpiar = document.querySelector(".mensajes-salida").classList.add('oculto');
+    document.querySelector(".mensajes-salida").classList.remove('ver');
+    // imgLimpiar.remove();
+    document.querySelector('.boton-copiar').classList.remove('oculto');
+
+
+    // let salidaTexto = document.querySelector(".salida-texto");
+    // entradaTexto.value = "";
+    // salidaTexto.value = "";
+
+    // let imgLimpiar=document.querySelector('.mensajes-salida');
+    // imgLimpiar.remove();
+}
+function limpiarDesencriptarCaja(){
+    document.querySelector('.salida-texto').innerHTML="";
+
+
+}
+
+function agregarClases(){
+    document.querySelector('.salida-texto').innerHTML="";
+    document.querySelector('.boton-copiar').classList.add('oculto');
+    document.querySelector('.mensajes-salida').classList.add('ver');
+}
+
+//accion de encripar
+let btnEncriptar = document.querySelector(".encriptar");
+    btnEncriptar.addEventListener("click", ()=> {
+        let entradaTexto = document.querySelector(".entrada-texto").value;
+        if(validar(entradaTexto)){
+            let msgError=document.querySelector('.msg-validar');
+            msgError.innerHTML="";
+            msgError.classList.add('oculto');
+            encriptar(entradaTexto);
+        }
+});
+//accion de desencriptar
+let btnDesecriptar = document.querySelector(".desencriptar");
+btnDesecriptar.addEventListener("click", function () {
+    let txtEntrada = document.querySelector(".entrada-texto").value;
+    
+    if(validar(txtEntrada)){
+        let msgError=document.querySelector('.msg-validar');
+        msgError.innerHTML="";
+        msgError.classList.add('oculto');
+        desencriptar(txtEntrada);
+
+    }
+});
+
+
+function procesoCodificar(mensaje,reemplazos){
+    for (let clave in reemplazos) {
+        mensaje = mensaje.replace(new RegExp(clave, "g"), reemplazos[clave]);
+    }
+    return mensaje;
+}
+
+
+function encriptar(mensaje) {
+    const diccionario = diccionarios();
+    const reemplazos = diccionario.reemplazosEncriptar;
+    const msgCodificado=procesoCodificar(mensaje,reemplazos);
+    document.querySelector(".salida-texto").textContent = msgCodificado;
+    limpiarEncriptarCaja();
+}
+
+function desencriptar(mensaje) {
+    const diccionario = diccionarios();
+    const reemplazos = diccionario.reemplazosDesencriptar;
+    const msgDesCodificado=procesoCodificar(mensaje,reemplazos);
+    document.querySelector(".salida-texto").textContent = msgDesCodificado;
+    limpiarEncriptarCaja();
+    // agregarClases();
+}
+//boton copiar
+
+let btnCopy = document
+    .querySelector(".boton-copiar")
+    .addEventListener("click", () => {
+        let textCopy = document.querySelector(".salida-texto").innerHTML;
+        copy(textCopy);
+        agregarClases();
+        
+        // document.querySelector('.boton-copiar').classList.add('oculto');
+
+        
+
+
+    });
+
+async function copy(text) {
+    try {
+        await navigator.clipboard.writeText(text);
+        console.log(text);
+        /* Resolved - text copied to clipboard successfully */
+    } catch (err) {
+        console.error("Failed to copy: ", err);
+        /* Rejected - text failed to copy to the clipboard */
+    }
+}
+
+function validar(texto){
+    let textMsgValidar=document.querySelector('.msg-validar');
+    
+    if(texto === ""){
+        textMsgValidar.classList.remove('oculto');
+        textMsgValidar.textContent="Por favor, escribe el mensaje a encriptar/desencriptar";
+        return false; // Detener la validación si el texto está vacío
+    }
+
+    if(!msgValidacion(texto)){
+        textMsgValidar.classList.remove('oculto');
+
+        textMsgValidar.textContent="El mensaje no debe tener mayúsculas o acentos";
+        return false; // Detener la validación si no pasa la validación de letras minúsculas
+    }
+
+    return true; // El texto ha pasado todas las validaciones
+}
+const msgValidacion = msg => {
+    return /^[a-z]+$/.test(msg); // Permitir letras minúsculas con acentos
+}
+
